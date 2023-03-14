3f1cf6066bf887aed4e833452663e17259f7b28e
Eli Finer <eli.finer@gmail.com>
2023-03-14 16:04:36 -0700

    switch to bash and simplify extract-audio

--- extract-audio.sh
+++ extract-audio.sh
@@ -1,12 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 if [ "$1" == "" ]; then
     echo "usage: $0 <file>"
     exit 1
 fi
 
-FILENAME=$1
-BASENAME={$1%.*}
+FILENAME="$1"
+BASENAME=${1%.*}
 
-ffmpeg -y -i $FILENAME -vn -acodec copy $BASENAME.aac
-ffmpeg -y -i $BASENAME.aac -sample_fmt s16 -ar 8000 $BASENAME.mp3
+ffmpeg -y -i "$FILENAME" -vn -ac 1 -ar 32000 "$BASENAME.low.mp3"
